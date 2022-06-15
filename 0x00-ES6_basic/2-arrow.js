aec3bc4629b197de9e7b5bcf88da05bf4eb29f08
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-15 12:00:04 +0300

    fix 2 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -3,8 +3,7 @@ export default function getNeighborhoodsList() {
 
   const self = this;
     self.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
-
 }
