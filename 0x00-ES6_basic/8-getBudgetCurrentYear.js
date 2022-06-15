24e0d35a3ce7d7ffc3f8785973f5787d4e0f74d9
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-15 18:05:22 +0300

    fix get year method

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,10 +1,13 @@
-const getCurrentYear = new Date().getFullYear();
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    [`income-${getCurrentYear}`]: income,
-    [`gdp-${getCurrentYear}`]: gdp,
-    [`capital-${getCurrentYear}`]: capita,
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capital-${getCurrentYear()}`]: capita,
   };
   return budget;
 }
