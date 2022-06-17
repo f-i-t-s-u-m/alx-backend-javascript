e7fdcc7909ff9e7c21b3fbacfd8c2e5828353ab5
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-18 02:08:00 +0300

    add final user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import signupUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all(signupUser(firstName, lastName), uploadPhoto(fileName))
+    .finally((x) => x);
+}
