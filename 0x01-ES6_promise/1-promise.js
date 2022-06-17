dd625e1a71cf170a1be8edf132d943dfd48c604f
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-17 23:17:43 +0300

    add 1 promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,7 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((res, rej) => {
-
-    if (success) res({ status: '200', body: 'Success' });
+    if (success) res({ status: 200, body: 'Success' });
     else rej(new Error('The fake API is not working currently'));
   });
 }
