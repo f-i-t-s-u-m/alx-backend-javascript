6de7ca3e7bb1a5a58fa4ee4277c02564853c55ad
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-21 22:17:04 +0300

    fix underscore

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 export default function cleanSet(set, startString) {
-  let app = '';
+  const app = [];
   set.forEach((x) => {
     if (x.startsWith(startString) && startString.length) {
-      if (app.length === 0) app = x.slice(startString.length);
-      else app += `_${x.slice(startString.length)}`;
+      app.push(x.slice(startString.length));
     }
   }, app);
-  return app;
+  return app.join('-');
 }
