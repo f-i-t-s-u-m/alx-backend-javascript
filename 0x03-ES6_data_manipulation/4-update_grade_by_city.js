eed2187eba2f462f18fb157efca3e35a77bda945
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-21 17:30:15 +0300

    refactor the code

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-param-reassign */
 export default function updateStudentGradeByCity(list, city, obj) {
   const a = list.filter((x) => x.location === city)
-    .reduce((acc, cur) => {
-      const grade = obj.find((f) => f.studentId === cur.id);
-      cur.grade = grade ? grade.grade : 'N/A';
-      acc.push(cur);
-      return acc;
-    }, []);
+    .map((y) => {
+      const grade = obj.find((f) => y.id === f.studentId);
+      y.grade = grade ? grade.grade : 'N/A';
+      return y;
+    });
   return a;
 }
