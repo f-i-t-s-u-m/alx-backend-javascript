312fc32ea20fefd3806a13d3ee4955fd63dd3a09
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-22 00:34:21 +0300

    add weakmap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,6 +2,6 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(obj) {
   if (!weakMap.has(obj)) weakMap.set(obj, 1);
-  else if (weakMap.get(obj) >= 5) throw Error('Endpoint load is high');
   else weakMap.set(obj, weakMap.get(obj) + 1);
+  if (weakMap.get(obj) >= 5) throw Error('Endpoint load is high');
 }
