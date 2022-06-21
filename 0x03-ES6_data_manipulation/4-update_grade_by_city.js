54f22ada28a65e55f8c4f8802505ad07cdda12f7
f-i-t-s-u-m <fibabirr@gmail.com>
2022-06-21 17:18:25 +0300

    fix null value for grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,9 @@
 export default function updateStudentGradeByCity(list, city, obj) {
   const a = list.filter((x) => x.location === city)
     .reduce((acc, cur) => {
-      obj.forEach((z) => {
-        if (z.studentId === cur.id) {
-          cur.grade = z.grade ? z.grade : 'N/A';
-          acc.push(cur);
-        }
-      });
+      const grade = obj.find((f) => f.studentId === cur.id);
+      cur.grade = grade ? grade.grade : 'N/A';
+      acc.push(cur);
       return acc;
     }, []);
   return a;
