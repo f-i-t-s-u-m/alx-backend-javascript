642837e95eb5e00d81f415f8c13cef780a169715
f-i-t-s-u-m <fibabirr@gmail.com>
2022-08-09 13:38:01 +0300

    add 2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  const db = {};
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const col = data.split('\n');
+    col.forEach((value, key) => {
+      if (key !== 0) {
+        const item = value.split(',');
+        if (db[item[3]] == null) db[item[3]] = [];
+        db[item[3]].push(item[0]);
+      }
+    });
+
+    for (const item in db) {
+      if (item) {
+        console.log(`Number of students in ${item}: ${db[item].length}. List: ${db[item].join(', ')}`);
+      }
+    }
+  });
+}
+
+module.exports = countStudents;
